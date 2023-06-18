9a49a2183588c0c29cf550fcee3d97bb6b1ab807
Anand-S23 <anandsroopsingh23@gmail.com>
2023-06-18 19:40:54 -0400

    Connecting to DB through pg pool

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,32 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { Pool } from 'pg';
 
 dotenv.config();
 
 const app: Express = express();
 app.use(cors<Request>());
 app.use(express.json());
+
 const port = process.env.PORT || 3001;
 
+const pool = new Pool({
+    user: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || 'password123',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: 5432,
+    database: process.env.POSTGRES_DB || 'shortest_db'
+});
+
 app.get('/', (req: Request, res: Response) => {
-    res.json({msg: 'Hello World'});
+    pool.query('SELECT * FROM shortest', (error, results) => {
+        if (error) {
+            res.status(500).json({'error': 'Error occured with database'});
+        }
+
+        res.status(200).json(results.rows);
+    });
 });
 
 app.post('/', (req: Request, res: Response) => {
